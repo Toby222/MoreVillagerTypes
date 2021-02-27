38a4e31e4a7ec8fa43442fe488a77c6721cc6e1e
Tobias Berger <tobi.berger13@gmail.com>
2021-02-27 11:40:46 +0100

    Add base workstation; Add textures

--- src/main/java/tech/tobot/morevillagers/module/Core.java
+++ src/main/java/tech/tobot/morevillagers/module/Core.java
@@ -4,10 +4,16 @@ import tech.tobot.morevillagers.MoreVillagers;
 import tech.tobot.morevillagers.base.ModModule;
 import tech.tobot.morevillagers.base.iface.Config;
 import tech.tobot.morevillagers.base.iface.Module;
+import tech.tobot.morevillagers.block.CustomWorkstation;
 import tech.tobot.morevillagers.client.CoreClient;
 
 @Module(mod = MoreVillagers.MOD_ID, client = CoreClient.class, alwaysEnabled = true, description = "Core configuration values.")
 public class Core extends ModModule {
     @Config(name = "Debug mode", description = "If true, routes additional debug messages into the standard game log.")
     public static final boolean DEBUG = false;
+
+    @Override
+    public void register() {
+        new CustomWorkstation(this, "base", null);
+    }
 }
